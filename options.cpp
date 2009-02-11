5b7bd875d5e8dffd89e1b30b91ae1bd0742c218e
f0x <fgrieco@gmail.com>
2009-02-11 10:28:55 +0100

    tooltip

--- options.cpp
+++ options.cpp
@@ -20,8 +20,13 @@
 #include "options.h"
 
 #include <Plasma/IconWidget>
+#include <Plasma/ToolTipManager>
+
 #include <QGraphicsLinearLayout>
 #include <KDebug>
+
+#include <KIcon>
+#include <KIconLoader>
 
 Options::Options(QGraphicsWidget *parent)
     : QGraphicsWidget(parent),
@@ -30,16 +35,30 @@ Options::Options(QGraphicsWidget *parent)
       m_loop(new Plasma::IconWidget(this))
 {
    m_tracklist->setIcon("format-list-unordered");
-   connect(m_tracklist, SIGNAL(clicked()), this, SLOT(playPauseClicked()));
+   connect(m_tracklist, SIGNAL(pressed(bool)), this, SLOT(playPauseClicked(bool)));
    m_tracklist->setMinimumSize(m_tracklist->sizeFromIconSize(10));
+
+   Plasma::ToolTipContent data;
+   data.setMainText(i18n("Tracklist"));
+   data.setSubText(i18n("View the tracklist"));
+   data.setImage(KIcon("format-list-unordered").pixmap(IconSize(KIconLoader::Desktop)));
+   Plasma::ToolTipManager::self()->setContent(m_tracklist, data);
 
    m_random->setIcon("roll");
    connect(m_random, SIGNAL(clicked()), this, SIGNAL(stop()));
    m_random->setMinimumSize(m_random->sizeFromIconSize(10));
+   data.setMainText(i18n("Random - off"));
+   data.setSubText(i18n("Play random track"));
+   data.setImage(KIcon("roll").pixmap(IconSize(KIconLoader::Desktop)));
+   Plasma::ToolTipManager::self()->setContent(m_random, data);
 
    m_loop->setIcon("object-rotate-right");
    connect(m_loop, SIGNAL(clicked()), this, SIGNAL(previous()));
    m_loop->setMinimumSize(m_loop->sizeFromIconSize(10));
+   data.setMainText(i18n("Repeat - off"));
+   data.setSubText(i18n("Play again the tracklist"));
+   data.setImage(KIcon("object-rotate-right").pixmap(IconSize(KIconLoader::Desktop)));
+   Plasma::ToolTipManager::self()->setContent(m_loop, data);
 
    QGraphicsLinearLayout *layout = new QGraphicsLinearLayout;
    layout->setOrientation(Qt::Vertical);
